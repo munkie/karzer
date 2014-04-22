b1a7e4c76bdb06a9a3eadc5439e8f9db7a04e700
Mikhail Shamin <munk13@gmail.com>
2014-04-22 15:34:58 +0400

    Get flowId from job test

--- src/Karzer/Util/Teamcity/TestListener.php
+++ src/Karzer/Util/Teamcity/TestListener.php
@@ -2,6 +2,7 @@
 
 namespace Karzer\Util\Teamcity;
 
+use Karzer\Framework\TestCase\JobTestInterface;
 use Karzer\Karzer;
 use PHPUnit\TeamCity\TestListener as TeamcityTestListener;
 use PHPUnit_Framework_Test;
@@ -9,11 +10,20 @@ use PHPUnit_Framework_Test;
 class TestListener extends TeamcityTestListener
 {
     /**
+     * @var string
+     */
+    protected $captureStandardOutput = 'false';
+
+    /**
      * @param PHPUnit_Framework_Test $test
      * @return int|void
      */
     protected function getFlowId(PHPUnit_Framework_Test $test)
     {
-        return Karzer::getThreadNumber();
+        if ($test instanceof JobTestInterface) {
+            return 'karzer-' . $test->getPoolPosition();
+        } else {
+            return 'phpunit-' . parent::getFlowId($test);
+        }
     }
 }
